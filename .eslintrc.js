dc272339abf352359382fd2a5730411eaa270e29
Daniel <daniel_ye.nz@hotmail.com>
2023-05-08 00:13:12 -0700

    start navbar

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,166 +4,41 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
+    "airbnb",
+    "plugin:prettier/recommended",
+    "plugin:react/jsx-runtime",
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
   rules: {
-
-    'no-console': 0,
-
-    'jsx-a11y/label-has-associated-control': 0,
-
-    'no-nested-ternary': 0,
-
-    'consistent-return': 0,
-
-    'no-alert': 0,
-
-    'react/jsx-no-constructed-context-values': 0,
-
-    'import/extensions': 0,
-
-    'react/prop-types': 0,
-
-    'linebreak-style': 0,
-
-    'react/state-in-constructor': 0,
-
-    'import/prefer-default-export': 0,
-
-    'react/react-in-jsx-scope': 'off',
-
-    'react/jsx-props-no-spreading': 'off',
-
-    'jsx-a11y/no-noninteractive-element-interactions': 'off',
-
-    'react/function-component-definition': [
-
-      2,
-
+    // Disable any conflicting rules or set them to a less strict level
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "react/jsx-props-no-spreading": 0,
+    "react/forbid-prop-types": 0,
+    "react/no-unescaped-entities": 0,
+    "react/jsx-no-useless-fragment": 0,
+    "jsx-a11y/anchor-is-valid": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
+    "jsx-a11y/label-has-associated-control": 0,
+    "no-console": 0,
+    "no-use-before-define": 0,
+    "no-nested-ternary": 0,
+    "no-plusplus": 0,
+    "no-underscore-dangle": 0,
+    "object-curly-newline": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "react/jsx-wrap-multilines": 0,
+    "react/prop-types": 0,
+    "react/require-default-props": 0,
+    "prettier/prettier": [
+      "error",
       {
-
-        namedComponents: 'arrow-function',
-
-        unnamedComponents: 'arrow-function',
-
+        endOfLine: "auto",
       },
-
     ],
-
-    'max-len': [
-
-      2,
-
-      550,
-
-    ],
-
-    'no-multiple-empty-lines': [
-
-      'error',
-
-      {
-
-        max: 1,
-
-        maxEOF: 1,
-
-      },
-
-    ],
-
-    'no-underscore-dangle': [
-
-      'error',
-
-      {
-
-        allow: [
-
-          '_d',
-
-          '_dh',
-
-          '_h',
-
-          '_id',
-
-          '_m',
-
-          '_n',
-
-          '_t',
-
-          '_text',
-
-        ],
-
-      },
-
-    ],
-
-    'object-curly-newline': 0,
-
-    'react/jsx-filename-extension': 0,
-
-    'react/jsx-one-expression-per-line': 0,
-
-    'jsx-a11y/click-events-have-key-events': 0,
-
-    'jsx-a11y/alt-text': 0,
-
-    'jsx-a11y/no-autofocus': 0,
-
-    'jsx-a11y/no-static-element-interactions': 0,
-
-    'react/no-array-index-key': 0,
-
-    'jsx-a11y/anchor-is-valid': [
-
-      'error',
-
-      {
-
-        components: [
-
-          'Link',
-
-        ],
-
-        specialLink: [
-
-          'to',
-
-          'hrefLeft',
-
-          'hrefRight',
-
-        ],
-
-        aspects: [
-
-          'noHref',
-
-          'invalidHref',
-
-          'preferButton',
-
-        ],
-
-      },
-
-    ],
-
   },
-
 };
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,27 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: 'Discover & Share AI Prompts'
-}
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({ children }) => {
+function RootLayout({ children }) {
   return (
-    <html Lang="en">
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
-            <main className="app">
-                {children}
-            </main>
-        </body>
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
     </html>
-  )
+  );
 }
 
-export default RootLayout
+export default RootLayout;

abc050297029cc6349b6a89d9eca516ef2034353
Daniel <daniel_ye.nz@hotmail.com>
2023-05-09 21:00:55 -0700

    start setup api route

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 import "@styles/globals.css";
 
 import Nav from "@components/Nav";
@@ -12,13 +13,15 @@ function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,17 @@
+"use client";
+
 import React from "react";
 
-function Provider() {
-  return <div>Provider</div>;
-}
+import { SessionProvider } from "next-auth/react";
+
+// eslint-disable-next-line react/function-component-definition
+const Provider = ({ children, session }) => {
+  // eslint-disable-next-line prettier/prettier
+  return(
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
+  )
+};
 
 export default Provider;

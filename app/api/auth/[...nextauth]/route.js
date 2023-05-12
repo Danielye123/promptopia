82edaf5c73ba1a648b4c2786ad3e49d3245e6b96
Daniel <daniel_ye.nz@hotmail.com>
2023-05-11 22:34:58 -0700

    setup googleprovider and start database connection

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,15 +5,17 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   async session({ session }) {
     //
   },
   async signIn({ profile }) {
-    //
+    try {
+      // serverLess route -> lambda function
+    } catch (error) { }
   },
 });
 
